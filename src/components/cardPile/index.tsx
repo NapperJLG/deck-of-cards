59ba45bcd221f5eaad58ca4bcf330dd7530dbcc8
Jake Napper <napperjlg@gmail.com>
2022-09-15 12:54:52 +0100

    fixed js error for array keys

--- src/components/cardPile/index.tsx
+++ src/components/cardPile/index.tsx
@@ -7,7 +7,7 @@ const CardPile = (props: { cards: CardProps[] }) => {
   return (
     <React.Fragment>
       <Container>
-        {props.cards.map((card) => <Card index={card.index} value={card.value} suit={card.suit} />)}
+        {props.cards.map((card) => <Card key={card.index} index={card.index} value={card.value} suit={card.suit} />)}
       </Container>
     </React.Fragment>
   )
