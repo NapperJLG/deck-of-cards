29373c222b4ad6265477c7809c9e32427b7c5487
Jake Napper <napperjlg@gmail.com>
2022-09-15 12:34:30 +0100

    button style tweaks

--- src/components/button/style.ts
+++ src/components/button/style.ts
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
 const Container = styled.button`
-  background-color: #800080;
+  background-color: #e60000;
   border: 1px solid black;
   height: 50px;
+  min-width: 100px;
   color: white;
   border-radius: 4px;
+  font-weight: 700;
   &:hover {
-    background-color: #660066;
+    background-color: #990000;
   }
+  padding: 0 10px;
 `
 
 export default Container
--- src/components/cardPile/style.ts
+++ src/components/cardPile/style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 175px;
+  margin-left: 145px;
   justify-content: center;
   display: flex;
   flex-direction: row-reverse;
