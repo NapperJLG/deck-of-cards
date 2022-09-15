02ed8d516a100f84a7a3e5156aab7a9d8289b99a
Jake Napper <napperjlg@gmail.com>
2022-09-15 09:00:24 +0100

    app structure refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Deck from './components/deck'
+import { CardProps } from './components/card'
 import Button from './components/button'
 
 function App() {
+
+  const [deck, setDeck] = useState<CardProps[]>([])
+
+  useEffect(() => {
+    const suits = ['clubs', 'diamonds', 'hearts', 'spades']
+    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    const tempDeck = []
+
+    for (let x = 0; x < suits.length; x++) {
+      for (let y = 0; y < values.length; y++) {
+        let card = { suit: suits[x], value: values[y] };
+        tempDeck.push(card);
+      }
+    };
+
+    const indexedDeck = tempDeck.map((card, i) => { return { ...card, index: i } })
+    setDeck(indexedDeck)
+  }, [])
+
   return (
     <div className="App">
-      <Deck />
-      <Button>
-        Shuffle
-      </Button>
+      <Deck deck={deck} />
+      <Button>Shuffle</Button>
     </div>
   );
 }
--- src/components/deck/index.tsx
+++ src/components/deck/index.tsx
@@ -1,32 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Card, { CardProps } from '../card'
 import Container from './style'
 
-const Deck = () => {
-
-  const [deck, setDeck] = useState<CardProps[]>([])
-
-
-  useEffect(() => {
-    const suits = ['clubs', 'diamonds', 'hearts', 'spades']
-    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    const tempDeck = []
-
-    for (let x = 0; x < suits.length; x++) {
-      for (let y = 0; y < values.length; y++) {
-        let card = { suit: suits[x], value: values[y] };
-        tempDeck.push(card);
-      }
-    };
-
-    const indexedDeck = tempDeck.map((card, i) => { return { ...card, index: i } })
-    setDeck(indexedDeck)
-  }, [])
+const Deck = (props: { deck: CardProps[] }) => {
 
   return (
-    <Container>
-      {deck.map((card) => <Card index={card.index} value={card.value} suit={card.suit} />)}
-    </Container>
+    <React.Fragment>
+      <Container>
+        {props.deck.map((card) => <Card index={card.index} value={card.value} suit={card.suit} />)}
+      </Container>
+    </React.Fragment>
   )
 
 }
