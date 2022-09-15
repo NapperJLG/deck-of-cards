228abdaca0ab42e2ea7de733dbf33d5ba21dd5d6
Jake Napper <napperjlg@gmail.com>
2022-09-15 11:37:25 +0100

    draw cards and sort drawn hand into suits

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import Deck from './components/deck'
+import CardPile from './components/cardPile'
 import { CardProps } from './components/card'
 import Button from './components/button'
 
 function App() {
 
   const [deck, setDeck] = useState<CardProps[]>([])
+  const [hand, setHand] = useState<CardProps[]>([])
 
   useEffect(() => {
+    createDeck()
+  }, [])
+
+  const createDeck = () => {
     const suits = ['clubs', 'diamonds', 'hearts', 'spades']
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     const tempDeck = []
@@ -21,10 +26,12 @@ function App() {
 
     const indexedDeck = tempDeck.map((card, i) => { return { ...card, index: i } })
     setDeck(indexedDeck)
-  }, [])
+
+    return indexedDeck
+  }
 
   const shuffleDeck = () => {
-    let array = deck
+    let array = createDeck()
     let currentIndex = array.length, randomIndex;
 
     while (currentIndex !== 0) {
@@ -36,12 +43,37 @@ function App() {
         array[randomIndex], array[currentIndex]];
     }
     setDeck([...array])
+    setHand([])
+  }
+
+  const drawCard = () => {
+    let tempDeck = deck
+    let tempHand = hand
+
+    var element = tempDeck[tempDeck.length - 1];
+    if (deck.length > 0) {
+      tempDeck.splice(tempDeck.length - 1, 1);
+      tempHand.push(element);
+
+    }
+
+    setDeck([...tempDeck])
+    setHand([...tempHand])
+  }
+
+  const sortHand = () => {
+    const tempHand = hand
+    tempHand.sort((a, b) => a.index - b.index).map((card, index, array) => card.value)
+    setHand([...tempHand])
   }
 
   return (
     <div className="App">
-      <Deck deck={deck} />
+      <CardPile cards={deck} />
       <Button onClick={() => shuffleDeck()}>Shuffle</Button>
+      <Button onClick={() => drawCard()}>Draw card</Button>
+      <Button onClick={() => sortHand()}>Sort Hand</Button>
+      <CardPile cards={hand} />
     </div>
   );
 }
--- src/components/cardPile/index.tsx
+++ src/components/cardPile/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Card, { CardProps } from '../card'
 import Container from './style'
 
-const Deck = (props: { deck: CardProps[] }) => {
+const CardPile = (props: { cards: CardProps[] }) => {
 
   return (
     <React.Fragment>
       <Container>
-        {props.deck.map((card) => <Card index={card.index} value={card.value} suit={card.suit} />)}
+        {props.cards.map((card) => <Card index={card.index} value={card.value} suit={card.suit} />)}
       </Container>
     </React.Fragment>
   )
 }
 
-export default Deck
+export default CardPile
